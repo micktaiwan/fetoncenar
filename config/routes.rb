4e0df6d394f73e2b1be15680b059cc72da01e639
Mickael Faivre-Maçon <faivrem@gmail.com>
2009-11-25 16:55:42 +0100

    more work

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "welcome"
 
   # See how all your routes lay out with "rake routes"
 
@@ -49,3 +49,4 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
+
--- db/migrate/20091125154838_create_users.rb
+++ db/migrate/20091125154838_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table "users", :force => true do |t|
-      t.column :login,                     :string, :limit => 40
       t.column :name,                      :string, :limit => 100, :default => '', :null => true
       t.column :email,                     :string, :limit => 100
       t.column :crypted_password,          :string, :limit => 40
@@ -13,10 +12,11 @@ class CreateUsers < ActiveRecord::Migration
 
 
     end
-    add_index :users, :login, :unique => true
+    add_index :users, :email, :unique => true
   end
 
   def self.down
     drop_table "users"
   end
 end
+
